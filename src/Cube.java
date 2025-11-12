21a1e65fac6499fd859172c3b4c54e48157ca753
nathanielayz2028 <nathanielayz2028@student.cis.edu.hk>
2025-11-12 11:16:21 +0800

    hi

---
+++ src/Cube.java
@@ -0,0 +1,33 @@
+public class Cube {
+    private int side;
+
+    public Cube() {
+        this.side = 1;
+    }
+    public Cube(int side) {
+        if (side < 1) {
+            throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
+        }
+        this.side = side;
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        if (side < 1) {
+            throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
+        }
+        this.side = side;
+    }
+
+    public int calculateSurfaceArea() {
+        return 6*side*side;
+    }
+
+    public int calculateVolume(){
+        return side*side*side;
+    }
+
+}
